a09f72ff38951aaddb92b0bd8839154f8a1d12f2
Matthew Blott <m4ttb@pm.me>
2024-01-10 13:37:20 +0000

    Minimalist approach

--- app/src/main/java/com/example/foo/features/web/WebFragment.kt
+++ app/src/main/java/com/example/foo/features/web/WebFragment.kt
@@ -1,19 +1,8 @@
 package com.example.foo.features.web
 
-import android.os.Bundle
-import android.view.View
 import dev.hotwire.turbo.fragments.TurboWebFragment
 import dev.hotwire.turbo.nav.TurboNavDestination
 import dev.hotwire.turbo.nav.TurboNavGraphDestination
-import com.example.foo.R
 
 @TurboNavGraphDestination(uri = "turbo://fragment/web")
-open class WebFragment : TurboWebFragment(), TurboNavDestination {
-
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-    toolbarForNavigation()?.inflateMenu(R.menu.web)
-
-  }
-  
-}
+open class WebFragment : TurboWebFragment(), TurboNavDestination
