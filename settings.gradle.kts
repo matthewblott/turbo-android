4648d59aa74fd3dc47051b41dc74714f0e1c399b
Matthew Blott <m4ttb@pm.me>
2024-05-28 16:48:26 +0100

    Changes to be committed: modified:   settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,3 @@
-// pluginManagement {
-//     repositories {
-//         google()
-//         mavenCentral()
-//         gradlePluginPortal()
-//     }
-// }
-// dependencyResolutionManagement {
-//     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-//     repositories {
-//         google()
-//         mavenCentral()
-//     }
-// }
-//
-// rootProject.name = "com.example.foo"
-// include(":app")
-
 pluginManagement {
   repositories {
     google()
