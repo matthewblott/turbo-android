56bea9f40def5bfa8b0eb47802af421840b32aa7
Matthew Blott <m4ttb@pm.me>
2024-05-28 16:01:28 +0100

    Trimmed it down to just the hotwire libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,13 @@
 plugins {
-  id("com.android.application")
-  id("org.jetbrains.kotlin.android")
+  id("com.android.application") version "8.4.1"
+  id("org.jetbrains.kotlin.android") version "1.9.0"
 }
 
 android {
   namespace = "com.example.foo"
   compileSdk = 34
-
   defaultConfig {
-    applicationId = "com.example.foo"
     minSdk = 34
-  }
-
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
   }
   kotlinOptions {
     jvmTarget = "1.8"
@@ -22,15 +15,6 @@ android {
 }
 
 dependencies {
-  implementation("androidx.core:core-ktx:1.12.0")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-  implementation("androidx.activity:activity-compose:1.8.2")
-  implementation(platform("androidx.compose:compose-bom:2023.10.01"))
-  implementation("androidx.compose.ui:ui")
-  implementation("androidx.compose.ui:ui-graphics")
-  implementation("androidx.compose.ui:ui-tooling-preview")
   implementation("dev.hotwire:turbo:7.0.2")
-  implementation("androidx.browser:browser:1.7.0")
-  implementation("com.google.android.material:material:1.11.0")
-  debugImplementation("androidx.compose.ui:ui-tooling")
+  implementation(platform("androidx.compose:compose-bom:2023.10.01"))
 }
